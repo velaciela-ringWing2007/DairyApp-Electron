dc358ee3c3cf92af19ec0e7b4f6c7f47c441c9e0
Nebukin <anachronism.coder@gmail.com>
2024-05-05 16:51:39 +0900

    Added feature to reside in the system tray

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, Tray, Menu } = require('electron');
 const path = require('path');
 
+let mainWindow;
+let tray = null;
+
 function createWindow() {
-    const mainWindow = new BrowserWindow({
+    mainWindow = new BrowserWindow({
         width: 1200,
         height: 800,
         title: "Diary",
@@ -18,14 +21,54 @@ function createWindow() {
     mainWindow.loadFile('index.html').then(() => {
         // mainWindow.webContents.openDevTools();
     });
+
+    // ウィンドウが閉じられた時に実行
+    mainWindow.on('close', (event) => {
+        if (!app.isQuitting) {
+            event.preventDefault();
+            mainWindow.hide();
+        }
+    });
 }
 
-app.whenReady().then(createWindow);
+function createTray() {
+    // タスクトレイアイコンのセットアップ
+    tray = new Tray(path.join(__dirname, 'assets/icons/tray-icon.png'));
+    const contextMenu = Menu.buildFromTemplate([
+        {
+            label: 'Open', click: () => {
+                mainWindow.show();
+            }
+        },
+        {
+            label: 'Quit', click: () => {
+                app.isQuitting = true;
+                app.quit();
+            }
+        }
+    ]);
+    tray.setToolTip('Diary App');
+    tray.setContextMenu(contextMenu);
+
+    // タスクトレイアイコンをクリックしたときにウィンドウを表示/非表示
+    tray.on('click', () => {
+        mainWindow.isVisible() ? mainWindow.hide() : mainWindow.show();
+    });
+}
+
+app.whenReady().then(() => {
+    createWindow();
+    createTray();
+});
 
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') app.quit();
 });
 
 app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow();
+    } else {
+        mainWindow.show();
+    }
 });
